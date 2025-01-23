00cdbd63f603bcd0b3856a41822814ed72af3c45
Presley McFarlane-Goolcharan <pmcfarlanegoolcharan@rrc.ca>
2025-01-23 14:30:37 -0600

    What I got done during the lecutre

--- module_02.py
+++ module_02.py
@@ -1,1 +1,29 @@
 """A module to demonstrate content from Module 2."""
+
+my_name = "Presley"
+my_age = 25
+print("My name is", my_name, "and I am", my_age, "years old")
+print(f"My name is {my_name} and I am {my_age} years old")
+absolute_value = abs(-12)
+print("absolute_value:", absolute_value)
+
+result = 1 * 3
+print("result:", result)
+print(f"result: {result}")
+
+#print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
+
+print("apples", "oranges", "bananas")
+print("apples", "oranges", "bananas", sep = ", ")
+
+value = 3.14159
+print(f"The value is {value:.2f}.")  # This is the rounding the value
+
+number = 123
+print(f"The number is {number:05}.")
+print(f"The number is {number:5}.")
+
+name = "John"
+print(f"Hello, {name:>10}.")
+print(f"Hello, {name:10}.")
+
